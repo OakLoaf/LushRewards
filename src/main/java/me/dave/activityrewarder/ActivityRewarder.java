bda0dc9affdaf0783a107f5fa66d4e80d9bfe298
BuildTools <cooldaveb@live.co.uk>
2023-02-04 13:28:31 +0000

    Made activityrewarder.use default permission

--- src/main/java/me/dave/activityrewarder/ActivityRewarder.java
+++ src/main/java/me/dave/activityrewarder/ActivityRewarder.java
@@ -1,7 +1,11 @@
 package me.dave.activityrewarder;
 
+import com.jeff_media.updatechecker.UpdateCheckSource;
+import com.jeff_media.updatechecker.UpdateChecker;
 import me.dave.activityrewarder.api.PlaceholderAPIHook;
 import me.dave.activityrewarder.events.RewardGUIEvents;
+import me.dave.chatcolorhandler.ChatColorHandler;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -40,6 +44,28 @@ public final class ActivityRewarder extends JavaPlugin {
 
         if (pluginManager.getPlugin("PlaceholderAPI") != null) new PlaceholderAPIHook().register();
         else getLogger().info("PlaceholderAPI plugin not found. Continuing without PlaceholderAPI.");
+
+//        new UpdateChecker(this, UpdateCheckSource.SPIGET, "107545")
+//            .setDownloadLink("https://www.spigotmc.org/resources/activity-rewarder.107545/")
+//            .setChangelogLink("https://www.spigotmc.org/resources/activity-rewarder.107545/updates")
+//            .onSuccess((commandSenders, latestVersion) -> {
+//                String currentVersion = getDescription().getVersion();
+//                if (latestVersion == currentVersion) return;
+//                for (CommandSender sender : (CommandSender[]) commandSenders) {
+//                    ChatColorHandler.sendMessage(sender, "&e&lRewards &8» &7Version &e" + latestVersion + "&7is available!");
+//                    ChatColorHandler.sendMessage(sender, "&e&lRewards &8» &7You are currently running on " + currentVersion);
+//                    ChatColorHandler.sendMessage(sender, "&e&lRewards &8» &7Download the latest version here:");
+//                    ChatColorHandler.sendMessage(sender, "&e&lRewards &8» &7https://www.spigotmc.org/resources/activity-rewarder.107545");
+//                }
+//            })
+//            .onFail((commandSenders, latestVersion) -> {
+//                getLogger().warning("Error when getting latest version");
+//            })
+//            .setNotifyByPermissionOnJoin("activityrewarder.updatechecker")
+//            .setNotifyRequesters(false)
+//            .suppressUpToDateMessage(true)
+//            .checkEveryXHours(0.5)
+//            .checkNow();
     }
 
     @Override
