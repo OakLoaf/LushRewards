11b928e9832523aee216fb9259c686a78fa1233b
BuildTools <cooldaveb@live.co.uk>
2023-08-02 13:51:52 +0100

    Shaded MorePaperLib into plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
     compileOnly("org.geysermc.floodgate:api:2.0-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.2")
-    compileOnly("space.arim.morepaperlib:morepaperlib:0.4.2")
+    shadow("space.arim.morepaperlib:morepaperlib:0.4.2")
     shadow(files("libs/EnchantedStorage.jar"))
     shadow("com.github.CoolDCB:ChatColorHandler:v1.3.3")
 }
