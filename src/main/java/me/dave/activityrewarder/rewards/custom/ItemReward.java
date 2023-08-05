637696207b7dc5d9928fd15dd652cafc0bc0b116
BuildTools <cooldaveb@live.co.uk>
2023-08-05 11:14:03 +0100

    Converted storage to SimpleItemStack

--- src/main/java/me/dave/activityrewarder/rewards/custom/ItemReward.java
+++ src/main/java/me/dave/activityrewarder/rewards/custom/ItemReward.java
@@ -1,8 +1,10 @@
 package me.dave.activityrewarder.rewards.custom;
 
+import me.dave.activityrewarder.ActivityRewarder;
 import me.dave.activityrewarder.rewards.Reward;
-import me.dave.activityrewarder.utils.ConfigParser;
+import me.dave.activityrewarder.utils.SimpleItemStack;
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -10,25 +12,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ItemReward implements Reward {
-    private final ItemStack itemStack;
+    private final SimpleItemStack simpleItemStack;
 
     public ItemReward(ItemStack itemStack) {
-        this.itemStack = itemStack;
+        this.simpleItemStack = new SimpleItemStack(itemStack);
     }
 
     public ItemReward(Map<?, ?> map) {
-        String material = map.containsKey("material") ? (String) map.get("material") : "e";
-        int amount = map.containsKey("amount") ? (int) map.get("amount") : 1;
+        ConfigurationSection configurationSection;
+        try {
+            configurationSection = (ConfigurationSection) map.get("items");
+        } catch(ClassCastException exc) {
+            ActivityRewarder.getInstance().getLogger().severe("Invalid config format at '" + map + "'");
+            this.simpleItemStack = new SimpleItemStack(Material.AIR);
+            return;
+        }
 
-        ItemStack item = ConfigParser.getItem(material.toUpperCase(), Material.GOLD_NUGGET);
-        item.setAmount(amount);
-
-        this.itemStack = item;
+        this.simpleItemStack = new SimpleItemStack(configurationSection);
     }
 
     @Override
     public void giveTo(Player player) {
-        HashMap<Integer, ItemStack> droppedItems = player.getInventory().addItem(itemStack);
+        HashMap<Integer, ItemStack> droppedItems = player.getInventory().addItem(simpleItemStack.getItemStack());
         for (ItemStack item: droppedItems.values()) {
             player.getWorld().dropItem(player.getLocation(), item);
         }
