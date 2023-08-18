bc0f4b6d04d818d0715ccd30d352449faac21d20
BuildTools <cooldaveb@live.co.uk>
2023-08-18 02:45:34 +0100

    Added SimpleDateParseException

---
+++ src/main/java/me/dave/activityrewarder/exceptions/SimpleDateParseException.java
@@ -0,0 +1,8 @@
+package me.dave.activityrewarder.exceptions;
+
+public class SimpleDateParseException extends RuntimeException {
+
+    public SimpleDateParseException(String message) {
+        super(message);
+    }
+}
