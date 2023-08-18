448624b19fa2b6c45a4ac161de18da51e4bdc94d
BuildTools <cooldaveb@live.co.uk>
2023-08-18 01:30:22 +0100

    Fixed item rewards not loading correctly under new format

--- src/main/java/me/dave/activityrewarder/rewards/custom/ItemReward.java
+++ src/main/java/me/dave/activityrewarder/rewards/custom/ItemReward.java
@@ -7,48 +7,40 @@ import me.dave.activityrewarder.utils.SimpleItemStack;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ItemReward implements Reward {
     private final SimpleItemStack simpleItemStack;
 
-    public ItemReward(Map<?, ?> map) throws InvalidRewardException {
-        Map<?, ?> itemMap;
-        try {
-            itemMap = map.containsKey("item") ? (Map<?, ?>) map.get("item") : Collections.emptyMap();
-        } catch(ClassCastException exc) {
-            ActivityRewarder.getInstance().getLogger().severe("Invalid config format at '" + map + "'");
-            throw new InvalidRewardException();
-        }
-
-        Material material = ConfigParser.getMaterial((String) itemMap.get("material"));
+    public ItemReward(@NotNull Map<?, ?> map) throws InvalidRewardException {
+        Material material = ConfigParser.getMaterial((String) map.get("material"));
 
         if (material != null) {
             this.simpleItemStack = new SimpleItemStack(material);
 
             try {
-                if (itemMap.containsKey("amount")) {
-                    simpleItemStack.setAmount((int) itemMap.get("amount"));
+                if (map.containsKey("amount")) {
+                    simpleItemStack.setAmount((int) map.get("amount"));
                 }
-                if (itemMap.containsKey("display-name")) {
-                    simpleItemStack.setDisplayName((String) itemMap.get("display-name"));
+                if (map.containsKey("display-name")) {
+                    simpleItemStack.setDisplayName((String) map.get("display-name"));
                 }
-                if (itemMap.containsKey("custom-model-data")) {
-                    simpleItemStack.setCustomModelData((int) itemMap.get("custom-model-data"));
+                if (map.containsKey("custom-model-data")) {
+                    simpleItemStack.setCustomModelData((int) map.get("custom-model-data"));
                 }
-                if (itemMap.containsKey("enchanted")) {
-                    simpleItemStack.setEnchanted((boolean) itemMap.get("enchanted"));
+                if (map.containsKey("enchanted")) {
+                    simpleItemStack.setEnchanted((boolean) map.get("enchanted"));
                 }
             } catch(ClassCastException exc) {
-                ActivityRewarder.getInstance().getLogger().severe("Invalid config format at '" + map + "'");
+                ActivityRewarder.getInstance().getLogger().severe("Invalid config format at '" + map + "', could not parse data");
                 throw new InvalidRewardException();
             }
         }
         else {
-            ActivityRewarder.getInstance().getLogger().severe("Invalid config format at '" + itemMap + "'");
+            ActivityRewarder.getInstance().getLogger().severe("Invalid config format at '" + map + "'");
             throw new InvalidRewardException();
         }
     }
