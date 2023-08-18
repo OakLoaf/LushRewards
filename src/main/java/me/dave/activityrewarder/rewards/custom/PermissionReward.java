430edbe49c324f6ef7450251599c62edefc2a6bc
BuildTools <cooldaveb@live.co.uk>
2023-08-18 10:46:50 +0100

    Implemented permission

--- src/main/java/me/dave/activityrewarder/rewards/custom/PermissionReward.java
+++ src/main/java/me/dave/activityrewarder/rewards/custom/PermissionReward.java
@@ -6,26 +6,28 @@ import org.bukkit.entity.Player;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 public class PermissionReward  implements Reward {
-    private static final Random random = new Random();
+    private final String permission;
     private final List<Reward> rewards;
 
     @SuppressWarnings("unchecked")
     public PermissionReward(Map<?, ?> map) {
         List<Map<?, ?>> rewardMaps;
+        permission = (String) map.get("permission");
         try {
             rewardMaps = map.containsKey("rewards") ? (List<Map<?, ?>>) map.get("rewards") : List.of(Collections.emptyMap());
         } catch(ClassCastException exc) {
             throw new InvalidRewardException("Invalid config format at '" + map + "'");
         }
-        
+
         this.rewards = Reward.loadRewards(rewardMaps, map.toString());
     }
 
     @Override
     public void giveTo(Player player) {
-        rewards.forEach(reward -> reward.giveTo(player));
+        if (player.hasPermission(permission)) {
+            rewards.forEach(reward -> reward.giveTo(player));
+        }
     }
 }
