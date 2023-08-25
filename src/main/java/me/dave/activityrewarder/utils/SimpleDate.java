397ddbecc548527d76a482b62772624e02eb11ef
BuildTools <cooldaveb@live.co.uk>
2023-08-25 03:43:08 +0100

    Added proper date handling including month changes and leap years

--- src/main/java/me/dave/activityrewarder/utils/SimpleDate.java
+++ src/main/java/me/dave/activityrewarder/utils/SimpleDate.java
@@ -10,12 +10,15 @@ import java.util.regex.Pattern;
 
 public class SimpleDate implements Cloneable {
     private static final Pattern DATE_FORMAT = Pattern.compile("([0-9]{1,2})/([0-9]{1,2})/([0-9]{4,})");
+    private static final int[] DAYS_PER_MONTH = new int[]{ 31, 29 /* 28 on all years except leap years */, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
     private int day;
     private int month;
     private int year;
 
     public SimpleDate(int day, int month, int year) {
-        if ((day < 0 || day > 31) || (month < 0 || month > 12) || (year < 0)) {
+        if ((day < 0 || (month < 0 || month > 12) || year < 0) || day > DAYS_PER_MONTH[month - 1]) {
+            throw new SimpleDateParseException("Invalid date");
+        } else if (day == 29 && month == 2 && year % 4 != 0) {
             throw new SimpleDateParseException("Invalid date");
         } else {
             this.day = day;
@@ -36,27 +39,51 @@ public class SimpleDate implements Cloneable {
         return year;
     }
 
-    // TODO: Needs to respect ends of months (and leap years)
     public void addDays(int days) {
-        boolean negative = days < 0;
+        days += this.day;
+        int months = 0;
 
-        int years = (int) Math.floor(days / 365.0);
-        this.year += years;
-
-        if (!negative) {
-            this.day += days % 365;
-        } else {
-            this.day -= days % 365;
+        for (int monthIndex = this.month - 1; days > DAYS_PER_MONTH[monthIndex] || (days > 28 && monthIndex == 1 && year % 4 == 0); monthIndex++) {
+            months++;
+            days -= DAYS_PER_MONTH[monthIndex];
         }
+        addMonths(months);
+
+        if (!validateDate(days, month, year)) throw new SimpleDateParseException("Invalid date");
+
+        this.day = days;
     }
 
-    // TODO: Needs to respect ends of years
     public void addMonths(int months) {
-        this.month += months;
+        int years = this.year + (int) Math.floor(months / (float) 12);
+        months = (months + this.month) % 12;
+        int days = Math.min(this.day, DAYS_PER_MONTH[months - 1]);
+
+        if (!validateDate(days, months, years)) throw new SimpleDateParseException("Invalid date");
+
+        this.month = months;
+        this.year = years;
+        this.day = days;
     }
 
     public void addYears(int years) {
+        if (!validateDate(day, month, year + years)) throw new SimpleDateParseException("Invalid date");
+
         this.year += years;
+    }
+
+    public boolean validateDate() {
+        return validateDate(day, month, year);
+    }
+
+    public boolean validateDate(int day, int month, int year) {
+        if ((day < 0 || (month < 0 || month > 12) || year < 0) || day > DAYS_PER_MONTH[month - 1]) {
+            return false;
+        } else if (day != 29 || month != 2 || year % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public String asString(String format) {
