b30c258b1935aff9fac3fe2e9473dc3c5652bc24
BuildTools <cooldaveb@live.co.uk>
2023-08-27 02:26:57 +0100

    Added getOrLoadRewardUser and saveAll methods

--- src/main/java/me/dave/activityrewarder/data/DataManager.java
+++ src/main/java/me/dave/activityrewarder/data/DataManager.java
@@ -1,7 +1,9 @@
 package me.dave.activityrewarder.data;
 
+`import me.dave.activityrewarder.ActivityRewarder;
+import me.dave.activityrewarder.api.event.RewardUserLoadEvent;
+import me.dave.activityrewarder.api.event.RewardUserUnloadEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
 import org.enchantedskies.EnchantedStorage.IOHandler;
 import org.jetbrains.annotations.NotNull;
@@ -10,7 +12,6 @@ import java.time.LocalDate;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
 
 public class DataManager {
     private final IOHandler<RewardUser> ioHandler = new IOHandler<>(new YmlStorage());
@@ -18,23 +19,47 @@ public class DataManager {
 
     public DataManager() {
         for (Player player : Bukkit.getOnlinePlayers()) {
-            loadRewardUser(player.getUniqueId()).thenAccept((rewardUser) -> rewardUser.setUsername(player.getName()));
+            getOrLoadRewardUser(player).thenAccept((rewardUser) -> rewardUser.setUsername(player.getName()));
         }
+    }
+
+    @NotNull
+    public CompletableFuture<RewardUser> getOrLoadRewardUser(@NotNull Player player) {
+        CompletableFuture<RewardUser> completableFuture = new CompletableFuture<>();
+
+        UUID uuid = player.getUniqueId();
+        RewardUser rewardUser = uuidToRewardUser.get(uuid);
+        if (rewardUser != null) {
+            completableFuture.complete(rewardUser);
+        } else {
+            loadRewardUser(uuid).thenAccept(completableFuture::complete);
+        }
+
+        return completableFuture;
     }
 
     public CompletableFuture<RewardUser> loadRewardUser(UUID uuid) {
         return ioHandler.loadPlayer(uuid).thenApply((rewardUser) -> {
             uuidToRewardUser.put(uuid, rewardUser);
+            Bukkit.getScheduler().runTask(ActivityRewarder.getInstance(), () -> ActivityRewarder.getInstance().callEvent(new RewardUserLoadEvent(rewardUser)));
             return rewardUser;
         });
     }
 
     public void unloadRewarderUser(UUID uuid) {
-        uuidToRewardUser.remove(uuid);
+        RewardUser rewardUser = uuidToRewardUser.get(uuid);
+
+        if (rewardUser != null && ActivityRewarder.getInstance().callEvent(new RewardUserUnloadEvent(rewardUser))) {
+            uuidToRewardUser.remove(uuid);
+        }
     }
 
     public void saveRewardUser(RewardUser rewardUser) {
         ioHandler.savePlayer(rewardUser);
+    }
+
+    public void saveAll() {
+        uuidToRewardUser.values().forEach(this::saveRewardUser);
     }
 
     @NotNull
@@ -55,9 +80,5 @@ public class DataManager {
 
     public IOHandler<RewardUser> getIoHandler() {
         return ioHandler;
-    }
-
-    private long getTicksToHours(long ticksPlayed) {
-        return TimeUnit.HOURS.convert(ticksPlayed * 50, TimeUnit.MILLISECONDS);
     }
 }
