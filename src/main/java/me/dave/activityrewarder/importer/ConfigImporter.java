53c66d2683d92e7170724294a89a2767e44108cb
BuildTools <cooldaveb@live.co.uk>
2023-09-04 19:11:31 +0100

    Refined import prep

--- src/main/java/me/dave/activityrewarder/importer/ConfigImporter.java
+++ src/main/java/me/dave/activityrewarder/importer/ConfigImporter.java
@@ -27,17 +27,28 @@ public abstract class ConfigImporter {
 
     @Nullable
     protected static File prepareForImport(File oldFile) {
-        if (!oldFile.renameTo(new File(ActivityRewarder.getInstance().getDataFolder(), oldFile.getPath() + "-old-" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy--HH-mm-ss")) + ".yml"))) {
+        return prepareForImport(oldFile, true);
+    }
+
+    @Nullable
+    protected static File prepareForImport(File oldFile, boolean createNew) {
+        File parent = oldFile.getParentFile();
+        String name = oldFile.getName();
+
+        if (!oldFile.renameTo(new File(parent, name + "-old-" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy--HH-mm-ss")) + ".yml"))) {
             ActivityRewarder.getInstance().getLogger().severe("Failed to rename file");
             return null;
         }
 
-        File newFile = new File(ActivityRewarder.getInstance().getDataFolder(), oldFile.getPath());
-        try {
-            newFile.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+        File newFile = new File(parent, name);
+
+        if (createNew) {
+            try {
+                newFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
         }
 
         return newFile;
