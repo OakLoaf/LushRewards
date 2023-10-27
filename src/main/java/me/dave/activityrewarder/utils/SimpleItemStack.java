b96de93ade9323ca26f5ce4a2611b8e771f55240
BuildTools <cooldaveb@live.co.uk>
2023-10-27 18:56:29 +0100

    Suppressed warnings

--- src/main/java/me/dave/activityrewarder/utils/SimpleItemStack.java
+++ src/main/java/me/dave/activityrewarder/utils/SimpleItemStack.java
@@ -18,6 +18,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@SuppressWarnings("unused")
 public class SimpleItemStack implements Cloneable {
     private Material material = null;
     private int amount = 1;
