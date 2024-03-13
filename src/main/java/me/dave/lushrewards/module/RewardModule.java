87feabeb0d992b6e27dcbc94688eda3250b40c9b
BuildTools <cooldaveb@live.co.uk>
2024-03-13 13:14:45 +0000

    Updated type ids

--- src/main/java/me/dave/lushrewards/module/RewardModule.java
+++ src/main/java/me/dave/lushrewards/module/RewardModule.java
@@ -41,9 +41,9 @@ public abstract class RewardModule extends Module {
     }
 
     public static class Type {
-        public static final String DAILY_REWARDS = "daily_rewards";
-        public static final String ONE_TIME_REWARDS = "one_time_rewards";
-        public static final String PLAYTIME_REWARDS = "playtime_rewards";
-        public static final String PLAYTIME_TRACKER = "playtime_tracker";
+        public static final String DAILY_REWARDS = "daily-rewards";
+        public static final String ONE_TIME_REWARDS = "one-time-rewards";
+        public static final String PLAYTIME_REWARDS = "playtime-rewards";
+        public static final String PLAYTIME_TRACKER = "playtime-tracker";
     }
 }
