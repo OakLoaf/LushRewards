34d1cfa1df898f814853d0cdc5bed0195965c2cb
BuildTools <cooldaveb@live.co.uk>
2024-03-13 15:36:18 +0000

    Added API for RewardTypes

--- src/main/java/me/dave/lushrewards/api/LushRewardsAPI.java
+++ src/main/java/me/dave/lushrewards/api/LushRewardsAPI.java
@@ -2,6 +2,8 @@ package me.dave.lushrewards.api;
 
 import me.dave.lushrewards.LushRewards;
 import me.dave.lushrewards.module.RewardModule;
+import me.dave.lushrewards.rewards.RewardTypes;
+import me.dave.lushrewards.rewards.custom.Reward;
 
 @SuppressWarnings("unused")
 public class LushRewardsAPI {
@@ -12,5 +14,13 @@ public class LushRewardsAPI {
 
     public static void unregisterRewardModuleType(String typeId) {
         LushRewards.getInstance().unregisterModuleType(typeId);
+    }
+
+    public static void registerRewardType(String type, Class<? extends Reward> rewardClass) {
+        RewardTypes.register(type, rewardClass);
+    }
+
+    public static void unregisterRewardType(String type) {
+        RewardTypes.unregister(type);
     }
 }
