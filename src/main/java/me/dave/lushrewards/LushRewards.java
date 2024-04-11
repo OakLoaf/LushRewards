80bb7e2bb7a7855c3f3b38aeee121cc271d38bda
Oak <cooldaveb@live.co.uk>
2024-04-11 22:24:19 +0100

    Moved modules disabling before DataManager

--- src/main/java/me/dave/lushrewards/LushRewards.java
+++ src/main/java/me/dave/lushrewards/LushRewards.java
@@ -95,14 +95,14 @@ public final class LushRewards extends SpigotPlugin {
             hooks = null;
         }
 
-        if (dataManager != null) {
-            dataManager.disable();
-            dataManager = null;
-        }
-
         if (modules != null) {
             unregisterAllModules();
             modules = null;
+        }
+
+        if (dataManager != null) {
+            dataManager.disable();
+            dataManager = null;
         }
 
         configManager = null;
