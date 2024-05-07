2c43399009ce18c261c6c896226e4645dd2ae77a
Oak <cooldaveb@live.co.uk>
2024-05-07 17:15:55 +0100

    Removed completed TODO

--- src/main/java/org/lushplugins/lushrewards/command/subcommand/EditUserSubCommand.java
+++ src/main/java/org/lushplugins/lushrewards/command/subcommand/EditUserSubCommand.java
@@ -16,7 +16,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-// TODO: Add playtime reward sub-commands
 public class EditUserSubCommand extends SubCommand {
 
     public EditUserSubCommand() {
