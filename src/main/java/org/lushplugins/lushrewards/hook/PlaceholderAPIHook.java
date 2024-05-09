de072dee8934296e281a83c1c90c80718497e366
Oak <cooldaveb@live.co.uk>
2024-05-09 15:37:47 +0100

    Added "lushrewards" identifier for placeholders, deprecated "rewarder"

--- src/main/java/org/lushplugins/lushrewards/hook/PlaceholderAPIHook.java
+++ src/main/java/org/lushplugins/lushrewards/hook/PlaceholderAPIHook.java
@@ -7,6 +7,7 @@ import org.jetbrains.annotations.NotNull;
 
 public class PlaceholderAPIHook extends Hook {
     private PlaceholderExpansion expansion;
+    private OutdatedPlaceholderExpansion outdatedExpansion;
 
     public PlaceholderAPIHook() {
         super(HookId.PLACEHOLDER_API.toString());
@@ -16,6 +17,9 @@ public class PlaceholderAPIHook extends Hook {
     public void onEnable() {
         expansion = new PlaceholderExpansion();
         expansion.register();
+
+        outdatedExpansion = new OutdatedPlaceholderExpansion();
+        outdatedExpansion.register();
     }
 
     @Override
@@ -24,9 +28,45 @@ public class PlaceholderAPIHook extends Hook {
             expansion.unregister();
             expansion = null;
         }
+
+        if (outdatedExpansion != null) {
+            outdatedExpansion.unregister();
+            outdatedExpansion = null;
+        }
     }
 
     public static class PlaceholderExpansion extends me.clip.placeholderapi.expansion.PlaceholderExpansion {
+
+        public String onPlaceholderRequest(Player player, @NotNull String params) {
+            return LushRewards.getInstance().getLocalPlaceholders().parsePlaceholder(params, player);
+        }
+
+        public boolean persist() {
+            return true;
+        }
+
+        public boolean canRegister() {
+            return true;
+        }
+
+        @NotNull
+        public String getIdentifier() {
+            return "lushrewards";
+        }
+
+        @NotNull
+        public String getAuthor() {
+            return LushRewards.getInstance().getDescription().getAuthors().toString();
+        }
+
+        @NotNull
+        public String getVersion() {
+            return LushRewards.getInstance().getDescription().getVersion();
+        }
+    }
+
+    @Deprecated(since = "3.0.0")
+    public static class OutdatedPlaceholderExpansion extends me.clip.placeholderapi.expansion.PlaceholderExpansion {
 
         public String onPlaceholderRequest(Player player, @NotNull String params) {
             return LushRewards.getInstance().getLocalPlaceholders().parsePlaceholder(params, player);
