df0a088f70d973fb1496535305a2637aeb191ebe
Oak <cooldaveb@live.co.uk>
2024-06-05 00:03:10 +0100

    Prepared converter classes Json to/from MySQL

---
+++ src/main/java/org/lushplugins/lushrewards/data/converter/JsonToMySQL.java
@@ -0,0 +1,14 @@
+package org.lushplugins.lushrewards.data.converter;
+
+// TODO
+public class JsonToMySQL extends Converter {
+
+    public JsonToMySQL(String name) {
+        super(name);
+    }
+
+    @Override
+    public boolean convert() {
+        return false;
+    }
+}
---
+++ src/main/java/org/lushplugins/lushrewards/data/converter/MySQLToJson.java
@@ -0,0 +1,14 @@
+package org.lushplugins.lushrewards.data.converter;
+
+// TODO
+public class MySQLToJson extends Converter {
+
+    public MySQLToJson(String name) {
+        super(name);
+    }
+
+    @Override
+    public boolean convert() {
+        return false;
+    }
+}
